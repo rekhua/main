54c64002e3039910e48690bbb62697beef79d0d4
Vasyl Mosiychuk <info@realestate.kh.ua>
2024-02-19 00:56:19 +0200

    Change #18

--- assets/js/main.h.js
+++ assets/js/main.h.js
@@ -6,5 +6,6 @@ sitemap: false
 {%- include js/popper.min.js -%}
 {%- include js/bootstrap.min.js -%}
 {%- include js/bootstrap.smoothscroll.min.js -%}
-{%- include js/ads.home.js -%}
+{%- include js/tom-select.base.min.js -%}
+{%- assign top = site.data.top -%}{%- if top.size > 0 -%}{%- include js/ads.home.js -%}{%- endif -%}
 {%- include js/realtyua.js -%}
